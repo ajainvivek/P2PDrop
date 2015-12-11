9f1f7ab01e9f02b0c59c8bef886f181f8b78d0b4
Ajain Vivek <avivek@deloitte.com>
2015-12-11 12:16:58 +0530

    Initial Commit

--- config/environment.js
+++ config/environment.js
@@ -2,10 +2,10 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'p2-pdrop',
+    modulePrefix: 'p2pdrop',
     environment: environment,
     baseURL: '/',
-    locationType: 'auto',
+    locationType: process.env.EMBER_CLI_ELECTRON ? 'hash' : 'auto',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
@@ -40,8 +40,17 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
   }
+
+  ENV.contentSecurityPolicy = {
+    'default-src': "'none'",
+    'script-src': "'self'",
+    'font-src': "'self'",
+    'connect-src': "'self'",
+    'img-src': "'self'",
+    'style-src': "'self'",
+    'media-src': "'self'"
+  };
 
   return ENV;
 };
