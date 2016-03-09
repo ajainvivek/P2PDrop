594fa2c34817e197fe01dd2af599a68d467b22f4
Ajain Vivek <ajainvivek07@gmail.com>
2016-03-09 10:48:18 +0530

    Contacts List Populated With Connected

--- app/controllers/home/sidebar.js
+++ app/controllers/home/sidebar.js
@@ -1,4 +1,7 @@
 import Ember from "ember";
+import config from '../../config/environment';
+import _array from 'lodash/array';
+import _collection from 'lodash/collection';
 
 const {
   Controller,
@@ -6,8 +9,19 @@ const {
 } = Ember;
 
 export default Controller.extend({
-  isContactSelected: true,
-  actions: {
+  users : inject.service("users"),
+  isContactSelected : true,
+  connectedUsers : [],
+  init : function () {
+    const uid = this.get('session.secure.uid');
+    let self = this;
+    this.get("users").getCurrentUser(uid).then(function (currentUser) {
+      let friends = currentUser.friends || {};
+      let connected = friends.connected || [];
+      self.set("connectedUsers", connected);
+    });
+  },
+  actions : {
     toggleMenu(title) {
       let isContactSelected = (title === "contacts") ? true : false;
       this.set("isContactSelected", isContactSelected);
